30dcc0660604163379e221d3ba0ca793f5bce72b
jhon sapuyes <jhonsapuyes1986@gmail.com>
2022-09-20 22:52:24 -0500

    haciendo ajustes en funcion post

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -1,8 +1,5 @@
 
 
-//const url= "https://g0fc1b3eeb2deea-reto2.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/client/client"
-//const url= "https://gedc07c6509dfcc-db.adb.us-phoenix-1.oraclecloudapps.com/ords/admin/client/client"
-
 var formulario = document.getElementById("form");
 
 form.addEventListener("submit", function(e){
@@ -38,83 +35,63 @@ form.addEventListener("submit", function(e){
 function get(pt_1) {
     const url= pt_1
     const api= new XMLHttpRequest();
-
     api.open("GET",url);
     api.send();
     api.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-
             let datos_api = JSON.parse(this.responseText);
             //console.log(datos_api.items)
             for(let item of datos_api.items){
                 console.log(item)
             }
         }
-
     }
 }
 
-function post(ruta,pt_1,pt_2,pt_3,pt_4) {
-    
+function post(ruta,pt_1,pt_2,pt_3,pt_4) {   
     const url_get= ruta
-    loadDoc(url_get, myFunction1)
-    function loadDoc(url, cFunction) {
-    const api= new XMLHttpRequest();
-
-    api.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-           let datos_api = JSON.parse(this.responseText);
-
-        //    let dato_get
-            for (let index = 0; index < 1; index++) {
-               let element = datos_api.items[index];
-               cFunction(element);
+    peticion_get(url_get, retorno_http)
+    function peticion_get(url, cFunction) {
+        const api= new XMLHttpRequest();
+        api.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+               let datos_api = JSON.parse(this.responseText);
+                for (let index = 0; index < 1; index++) {
+                   let element = datos_api.items[index];
+                   cFunction(element);
+                }
             }
-        //    work_date= dato_get
         }
-        //ss=work_date
-        //console.log(work_date)
-        //console.log(this.responseText)
+        api.open("GET",url_get);
+        api.send();
     }
-    api.open("GET",url_get);
-    api.send();
-}
-function myFunction1(xhttp) {
-console.log(xhttp)
-  }
-    
-//console.log(ss)
-    //var contact = new Object();
-    //contact.id = pt_1;
-    //contact.name = pt_2;
-    //contact.email = pt_3;
-    //contact.age = pt_4;
-
-    //var contact = {
-    //    make: 'Ford',
-    //    model: 'Mustang',
-    //    year: 1969
-    //};
-        
-    //var data = JSON.stringify(contact);
-
-
-    const url=ruta
-    var xhr= new XMLHttpRequest();
-    xhr.open('POST', url,true)
-    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
-
-    // PARA AUTORIZAR CON USUARIO Y CONTRASEÑA
-    //var autorizacion = btoa("ADMIN:Estoesreto02");
-    //xhr.setRequestHeader("Authorization","Basic "+ autorizacion);
-
-    //xhr.send(data);
-    //
-    //xhr.onload = function () {
-    //    if(xhr.status === 201) {
-    //        traer_info(ruta)
-    //    }
-    //}
+    function retorno_http(xhttp) {
+        let at= xhttp
+        let casillas_post=[]
+        Object.entries(at).map(function(casilla) {
+            //console.log(casilla[0]); 
+            casillas_post.push(casilla[0])
+        })
+        let postear = new Object()
+        let datos_post=[pt_1,pt_2,pt_3,pt_4]
+        for (let i = 0; i < casillas_post.length; i++) {
+            const element = casillas_post[i];
+            text="postear."+element+"='"+ datos_post[i]+"'";
+            eval(text)
+        }
+        //console.log(casillas_post.length,casillas_post); 
+        let data = JSON.stringify(postear)
+        const url=ruta
+        var xhr= new XMLHttpRequest();
+        xhr.open('POST', url,true)
+        xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+        xhr.send(data);
+        xhr.onload = function () {
+            if(xhr.status === 201) {
+                get(ruta)        
+            }
+        }
+    }
 }
 
 function traer_info(pt_1){
